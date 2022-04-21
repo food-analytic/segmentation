d714d818daef9f5c06954fd443aa30b39f51e18d
KombangkoeDias <kbdirifilation@gmail.com>
2022-04-21 08:35:52 +0700

    add config

--- Segmentation/__init__.py
+++ Segmentation/__init__.py
@@ -1,9 +1,10 @@
-from .SegmentationUtils import StaticUtils, SegmentationTrainWrapper, create_datasets, Utils
+from .SegmentationUtils import StaticUtils, SegmentationTrainWrapper, create_datasets, Utils, config
 
 
 __all__ = [
     'StaticUtils',
     'SegmentationTrainWrapper',
     'create_datasets',
-    'Utils'
+    'Utils',
+    'config'
 ]
